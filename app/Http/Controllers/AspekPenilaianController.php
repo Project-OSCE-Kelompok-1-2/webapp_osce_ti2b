682f0307fca054c63760463b55fdd9eee3cc2e24
ifad <yusuffadhlih123@gmail.com>
2025-10-30 15:33:31 +0700

    Menambah aspek penilaian controller dan menambahkan route get aspek penilaian per stase

---
+++ app/Http/Controllers/AspekPenilaianController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Models\AspekPenilaian;
+
+class AspekPenilaianController extends Controller
+{
+    // mengambil aspek penilaian dari id stase
+    public function get_aspek_penilaian($id)
+    {
+        $id_stase = $id;
+        $aspek_penilaian = AspekPenilaian::where("id_stase", $id_stase)->withCount("poinAspekPenilaian as jumlah_kompetensi")->get();
+
+        return Inertia::render("Admin/AspekPenilaian", ["aspek_penilaian" => $aspek_penilaian]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AspekPenilaianController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia("Home");
+});
+
+Route::middleware(["auth", "role:admin"])->group(function () {
+    Route::get('/stase/{id}/aspek', [AspekPenilaianController::class, "get_aspek_penilaian"]);
 });
