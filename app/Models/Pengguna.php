d5df9cecf56a3cfef2f4bf4445726135e7c55a3c
setyanoegraha <pandu4688@gmail.com>
2025-10-30 16:51:18 +0700

    refactor(auth): ubah model Pengguna agar mendukung sistem autentikasi Laravel

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Pengguna extends Model
+class Pengguna extends Authenticatable
 {
     use HasFactory;
 
