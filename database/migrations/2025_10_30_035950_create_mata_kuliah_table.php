0cbfb9793b44de16e1bccb5ab6d0618a28a1f64a
najwarahma <hapsarinajwarahma@gmail.com>
2025-10-30 11:51:48 +0700

    tambah file migrasi untuk tabel mata kuliah, id enrollment belum direlasikan

---
+++ database/migrations/2025_10_30_035950_create_mata_kuliah_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mata_kuliah', function (Blueprint $table) {
+            $table->id('id_mata_kuliah');
+            $table->foreignId('id_blok')->constrained('blok')->onDelete('cascade');
+            // $table->foreignId('id_enrollment')->nullable()->constrained('enrollment')->onDelete('cascade');
+            $table->string('nama_mata_kuliah', 150);
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mata_kuliah');
+    }
+};
