a9786f7b8ad04d4b8492cfb914f51427ca76c37b
najwarahma <hapsarinajwarahma@gmail.com>
2025-10-30 11:53:13 +0700

    tambah file migrasi untuk tabel stase

---
+++ database/migrations/2025_10_30_043018_create_stase_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stase', function (Blueprint $table) {
+            $table->id('id_stase');
+            $table->foreignId('id_mata_kuliah')->constrained('mata_kuliah')->onDelete('cascade');
+            $table->foreignId('id_tujuan_pembelajaran')->nullable()->constrained('tujuan_pembelajaran')->onDelete('set null');
+            $table->string('nama_stase', 150);
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stase');
+    }
+};
