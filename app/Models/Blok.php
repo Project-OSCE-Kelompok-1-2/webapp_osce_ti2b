c430fd5c5b5f6457eea582d1d6325f59db98081e
setyanoegraha <pandu4688@gmail.com>
2025-10-31 10:02:44 +0700

    fix(model): membenarkan typo HasFactory

--- app/Models/Blok.php
+++ app/Models/Blok.php
@@ -2,13 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactories;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Blok extends Model
 {
     use HasFactory;
-    
+
     protected $table = 'blok';
     protected $primaryKey = 'id_blok';
     protected $fillable = ['nama_blok', 'deskripsi'];
