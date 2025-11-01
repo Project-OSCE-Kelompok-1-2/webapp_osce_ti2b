f53bd4b1560ddef23ccb73e83c6931d8caf8c37a
ifad <yusuffadhlih123@gmail.com>
2025-11-01 15:25:32 +0700

    mengubah get aspek penilaian agar mengirimkan data nama stase ke front end

--- app/Http/Controllers/AspekPenilaianController.php
+++ app/Http/Controllers/AspekPenilaianController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Models\AspekPenilaian;
+use App\Models\Stase;
 
 class AspekPenilaianController extends Controller
 {
-    // mengambil aspek penilaian dari id stase
+    // mengambil aspek penilaian menggunakan id stase
     public function get_aspek_penilaian($id)
     {
-        $id_stase = $id;
-        $aspek_penilaian = AspekPenilaian::where("id_stase", $id_stase)->withCount("poinAspekPenilaian as jumlah_kompetensi")->get();
+        $stase = Stase::findOrFail($id);
 
-        return Inertia::render("Admin/AspekPenilaian", ["data" => $aspek_penilaian]);
+        $aspek_penilaian = AspekPenilaian::where("id_stase", $id)
+            ->withCount("poinAspekPenilaian as jumlah_kompetensi")
+            ->get();
+
+        $data = [
+            "nama_stase" => $stase->nama_stase,
+            "aspek_penilaian" => $aspek_penilaian,
+        ];
+
+        return Inertia::render("Admin/AspekPenilaian", [
+            "data" => $data,
+        ]);
     }
 }
