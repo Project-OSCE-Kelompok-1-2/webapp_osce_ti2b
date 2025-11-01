87c4d95de40fc84b87bb376a5add227323ef5159
ifad <yusuffadhlih123@gmail.com>
2025-11-01 20:34:48 +0700

    menggunakan alias pada nama kolom yang dikirim ke front end

--- app/Http/Controllers/AspekPenilaianController.php
+++ app/Http/Controllers/AspekPenilaianController.php
@@ -12,14 +12,17 @@ class AspekPenilaianController extends Controller
     // mengambil aspek penilaian menggunakan id stase
     public function get_aspek_penilaian(Request $request, $id)
     {
+        // Pastikan Stase ditemukan, atau lempar 404
         $stase = Stase::findOrFail($id);
+
         // kueri parameter yang dikirim frontend untuk mencari aspek penilaian tertentu
-        $keyword = $request->query('search');
+        $search = $request->query('search');
 
         // jika ada keyword maka kirim aspek penilaian tertentu, jika tidak ada maka kirim semua aspek penilaian
-        $aspek_penilaian = AspekPenilaian::where('id_stase', $id)
-            ->when($keyword, function ($query, $keyword) {
-                $query->where('aspek', 'like', "%{$keyword}%");
+        $aspek_penilaian = AspekPenilaian::where('id_stase', $id)->select("id_aspek_penilaian as id", "aspek as nama", "bobot_maksimum")
+            ->when($search, function ($query, $search) {
+                // Menggunakan LIKE untuk pencarian parsial
+                $query->where('aspek', 'like', "%{$search}%");
             })
             ->withCount('poinAspekPenilaian as jumlah_kompetensi')
             ->get();
@@ -27,10 +30,9 @@ class AspekPenilaianController extends Controller
         $data = [
             "nama_stase" => $stase->nama_stase,
             "aspek_penilaian" => $aspek_penilaian,
-            "search" => $keyword,
         ];
 
-        return Inertia::render("Admin/AspekPenilaian", [
+        return Inertia::render("Testing", [
             "data" => $data,
         ]);
     }
