4453ed7daa41e6de011b2dffd02fad49c99ceae1
ifad <yusuffadhlih123@gmail.com>
2025-11-01 17:22:10 +0700

    menambahkan kueri parameter untuk filter aspek penilaian

--- app/Http/Controllers/AspekPenilaianController.php
+++ app/Http/Controllers/AspekPenilaianController.php
@@ -10,17 +10,24 @@ use App\Models\Stase;
 class AspekPenilaianController extends Controller
 {
     // mengambil aspek penilaian menggunakan id stase
-    public function get_aspek_penilaian($id)
+    public function get_aspek_penilaian(Request $request, $id)
     {
         $stase = Stase::findOrFail($id);
+        // kueri parameter yang dikirim frontend untuk mencari aspek penilaian tertentu
+        $keyword = $request->query('q');
 
-        $aspek_penilaian = AspekPenilaian::where("id_stase", $id)
-            ->withCount("poinAspekPenilaian as jumlah_kompetensi")
+        // jika ada keyword maka kirim aspek penilaian tertentu, jika tidak ada maka kirim semua aspek penilaian
+        $aspek_penilaian = AspekPenilaian::where('id_stase', $id)
+            ->when($keyword, function ($query, $keyword) {
+                $query->where('aspek', 'like', "%{$keyword}%");
+            })
+            ->withCount('poinAspekPenilaian as jumlah_kompetensi')
             ->get();
 
         $data = [
             "nama_stase" => $stase->nama_stase,
             "aspek_penilaian" => $aspek_penilaian,
+            "search" => $keyword,
         ];
 
         return Inertia::render("Admin/AspekPenilaian", [
