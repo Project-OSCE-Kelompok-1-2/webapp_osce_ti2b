ef8fb35ec4dade04dcdaa7acba4f215dd2f161ed
ifad <yusuffadhlih123@gmail.com>
2025-11-06 17:34:00 +0700

    Membuat OsceStaseController yang berisi fungsi index dan store

---
+++ resources/js/pages/Stase.jsx
@@ -0,0 +1,10 @@
+export default function Stase({ data }) {
+    console.log(data);
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <button></button>
+        </div>
+    );
+}
