aaf8e448eac80da1755d9ab09d78e04a8b83885a
HfzhImn <hafizhimanwicaksono@gmail.com>
2025-11-08 14:52:05 +0700

    update file halaman rekapsesi agar kolom seacrh panjangnya sesuai dengan desain

--- resources/js/pages/Admin/RekapSesiPage.jsx
+++ resources/js/pages/Admin/RekapSesiPage.jsx
@@ -81,7 +81,7 @@ export default function RekapSesiPage() {
                       (HANYA search bar dan tombol)
                     */}
                     <div className="flex flex-col md:flex-row justify-between items-center gap-4 mb-5">
-                        <div className="relative w-full md:w-96">
+                        <div className="relative w-full md:flex-1">
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <Search className="h-5 w-5 text-gray-400" />
                             </div>
