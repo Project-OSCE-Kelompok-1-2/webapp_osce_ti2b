ea6ea33c3c106bc066fce4a4c755f7b7fe990184
Ray Egans <egansray@gmail.com>
2025-11-18 09:52:12 +0700

    Feat: menambahkan fitur API untuk controller Auth

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+// 1. Import trait Sanctum di sini
+use Laravel\Sanctum\HasApiTokens; // <--- TAMBAHKAN INI
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Pengguna extends Authenticatable
 {
-    use HasFactory;
+    // 2. Gunakan trait tersebut di dalam class
+    use HasApiTokens, HasFactory; // <--- DAN INI (tambahkan HasApiTokens)
 
     protected $table = 'pengguna';
     protected $primaryKey = 'id_pengguna';
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+
+Route::prefix('v1')->group(function () {
+    Route::get('/login', function () {
+        return redirect()->route('login'); 
+    });
+
+    // 2. Route API Asli (POST)
+    // Ini yang dipakai Aplikasi Mobile
+    Route::post('/login', [AuthController::class, 'login']);
+
+    // Routes yang butuh Token (Protected)
+    Route::middleware('auth:sanctum')->group(function () {
+        
+        Route::post('/logout', [AuthController::class, 'logout']);
+        
+        // Cek user saat ini
+        Route::get('/me', function (Request $request) {
+            return $request->user();
+        });
+    });
+
+});
