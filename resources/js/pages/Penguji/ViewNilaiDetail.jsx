8f6243ebb6ca1de28e439b5f1a8fb5cdec077e2c
HfzhImn <hafizhimanwicaksono@gmail.com>
2025-11-20 13:01:51 +0700

    Update ViewNilaiDetail.jsx

--- resources/js/pages/Penguji/ViewNilaiDetail.jsx
+++ resources/js/pages/Penguji/ViewNilaiDetail.jsx
@@ -1,200 +1,143 @@
-import React, {useState} from "react";
-export default (props) => {
-	const [input1, onChangeInput1] = useState('');
-	const [input2, onChangeInput2] = useState('');
-	const [input3, onChangeInput3] = useState('');
-	return (
-		<div className="items-start bg-white">
-			<div className="flex flex-col items-start bg-white w-[1440px]">
-				<div className="flex flex-col self-stretch bg-white pb-0.5 mt-6 mb-2.5 mx-6 gap-5">
-					<div className="flex items-start self-stretch gap-[13px]">
-						<img
-							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/mvu5av4h_expires_30_days.png"} 
-							className="w-[54px] h-[54px] object-fill"
-						/>
-						<input
-							placeholder={"OSCE / OSCE Radiologi 01-A / Detail OSCE/Detail Stase/ Penilaian Stase/Lihat Penilaian"}
-							value={input1}
-							onChange={(event)=>onChangeInput1(event.target.value)}
-							className="text-black bg-white text-xl w-[1324px] py-[15px] px-[19px] rounded-xl border border-solid border-black"
-						/>
-					</div>
-					<img
-						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/0myi0jkh_expires_30_days.png"} 
-						className="self-stretch h-[1px] object-fill"
-					/>
-				</div>
-				<div className="flex items-center self-stretch py-3 pl-5 mb-3 mx-[53px] rounded-xl border border-solid border-black">
-					<img
-						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/9934ah6y_expires_30_days.png"} 
-						className="w-[85px] h-[85px] mr-[17px] object-fill"
-					/>
-					<div className="w-[177px] mr-[291px]">
-						<div className="flex flex-col items-center self-stretch mb-[17px] mx-[1px]">
-							<span className="text-black text-sm" >
-								{"Nama : Putri Levina Agatha"}
-							</span>
-						</div>
-						<div className="flex flex-col items-center self-stretch mb-5 ml-[1px] mr-[26px]">
-							<span className="text-black text-sm" >
-								{"NIM: 12345689012345"}
-							</span>
-						</div>
-						<div className="flex flex-col items-center self-stretch mr-[37px]">
-							<span className="text-black text-sm" >
-								{"Jurusan : Kedokteran"}
-							</span>
-						</div>
-					</div>
-				</div>
-				<div className="flex flex-col items-start pb-[1px] mb-3 ml-[53px]">
-					<span className="text-black text-xl" >
-						{"Penilaian Stase"}
-					</span>
-				</div>
-				<div className="flex items-start self-stretch bg-white py-[5px] px-3 mb-3 ml-[53px] mr-[65px] gap-2.5 rounded-xl border border-solid border-black">
-					<div className="flex flex-col items-start w-[51px] py-[11px] px-4">
-						<span className="text-black text-[15px]" >
-							{"No"}
-						</span>
-					</div>
-					<div className="flex flex-col items-start w-[523px] py-[11px] px-[206px]">
-						<span className="text-black text-[15px]" >
-							{"Aspek Penilaian"}
-						</span>
-					</div>
-					<div className="flex flex-col items-start w-[292px] py-[11px] px-[130px]">
-						<span className="text-black text-[15px]" >
-							{"Skor"}
-						</span>
-					</div>
-					<div className="flex flex-col items-start w-[206px] py-[11px] px-[83px]">
-						<span className="text-black text-[15px]" >
-							{"Bobot"}
-						</span>
-					</div>
-					<div className="flex flex-col items-start w-[186px] py-[11px] px-[79px]">
-						<span className="text-black text-[15px]" >
-							{"Nilai"}
-						</span>
-					</div>
-				</div>
-				<div className="flex flex-col self-stretch p-2.5 mx-[53px] gap-2.5" 
-					style={{
-						boxShadow: "0px 4px 4px #00000040"
-					}}>
-					<div className="flex flex-col items-start self-stretch bg-white py-4 px-3 rounded-xl border border-solid border-black">
-						<span className="text-black text-[15px] font-bold" >
-							{"A. Persiapan"}
-						</span>
-					</div>
-					<div className="flex items-center self-stretch bg-white rounded-xl">
-						<div className="flex flex-col items-start w-[52px] py-[30px] pl-3 mr-2.5">
-							<span className="text-black text-[22px]" >
-								{"1"}
-							</span>
-						</div>
-						<div className="w-[9px] py-[13px] mr-2.5">
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<span className="text-black text-[15px]" >
-							{"Verifikasi"}
-						</span>
-						<div className="flex-1 self-stretch">
-						</div>
-						<div className="w-0.5 py-[13px] mr-3.5">
-							<div className="self-stretch h-[61px]">
-							</div>
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<div className="flex items-start w-[274px] p-2.5 mr-3.5 gap-[26px]">
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/sw54ew2e_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/elvdewr4_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/lny2i3gm_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/s8kgbp25_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/de1nbm1p_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-						</div>
-						<div className="w-[1px] py-[13px] mr-2.5">
-							<div className="self-stretch h-[61px]">
-							</div>
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<div className="flex flex-col items-start w-[195px] py-[35px] px-[93px] mr-2.5">
-							<span className="text-black text-[15px]" >
-								{"3"}
-							</span>
-						</div>
-						<div className="w-0.5 py-[13px] mr-2.5">
-							<div className="self-stretch h-[61px]">
-							</div>
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<div className="flex flex-col items-start w-[195px] py-[35px] px-[89px]">
-							<span className="text-black text-[15px] font-bold" >
-								{"12"}
-							</span>
-						</div>
-					</div>
-					<div className="flex items-center self-stretch bg-[#D9D9D9] rounded-xl">
-						<div className="flex flex-col items-start w-[52px] py-[30px] pl-3 mr-2.5">
-							<span className="text-black text-[22px]" >
-								{"2"}
-							</span>
-						</div>
-						<div className="w-[9px] py-[13px] mr-2.5">
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<span className="text-black text-[15px]" >
-							{"Menyiapkan Alat"}
-						</span>
-						<div className="flex-1 self-stretch">
-						</div>
-						<div className="w-0.5 py-[13px] mr-2.5">
-							<div className="self-stretch h-[61px]">
-							</div>
-							<div className="self-stretch bg-[#00000080] h-[61px]">
-							</div>
-						</div>
-						<div className="flex items-start w-[282px] py-6 px-3.5 mr-2.5 gap-[26px]">
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/qtthyr7r_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/7kljc0cr_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/fxmmc5xe_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/64llh4le_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-							<img
-								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/I1JF6HlbfB/5sbalie4_expires_30_days.png"} 
-								className="w-[30px] h-10 object-fill"
-							/>
-						</div>
-						<div className="w-[1px] py-[13px] mr-2.5"></div>
+import React from "react";
+import { Link, usePage } from "@inertiajs/react";
+import { ArrowLeft } from "lucide-react";
+
+// --- Import Komponen ---
+import Sidebar from "../../components/Sidebar";
+import OsHeader from "../../components/Header";
+import OsCopyright from "../../components/copyright";
+
+// --- Mock Data (Bisa diganti data dari DB) ---
+const mockData = {
+  mahasiswa: {
+    nama: "Putri Levina Agatha",
+    nim: "12345689012345",
+    jurusan: "Kedokteran"
+  },
+  penilaian: [
+    {
+      kategori: "A. Persiapan",
+      items: [
+        { id: 1, aspek: "Verifikasi", skor: 4, bobot: 3, nilai: 12 },
+        { id: 2, aspek: "Menyiapkan Alat", skor: 4, bobot: 3, nilai: 12 },
+        { id: 3, aspek: "Cuci Tangan 6 Langkah", skor: 4, bobot: 3, nilai: 12 },
+      ]
+    },
+    {
+      kategori: "B. Orientasi",
+      items: [
+        { id: 1, aspek: "Verifikasi", skor: 4, bobot: 3, nilai: 12 },
+      ]
+    }
+  ],
+  totalNilai: 48,
+  feedback: "Lorem ipsum dolor sit amet consectetur. Sapien porttitor urna nibh a urna. Sodales nam mollis iaculis diam viverra. Arcu a ligula morbi tristique suscipit amet. Nibh tincidunt eget aliquet vulputate tempus quisque magna.Lorem ipsum dolor sit amet consectetur. Sapien porttitor urna nibh a urna. Sodales nam mollis iaculis diam viverra. Arcu a ligula morbi tristique suscipit amet. Nibh tincidunt eget aliquet vulputate tempus quisque magna.Lorem ipsum dolor sit amet consectetur. Sapien porttitor urna nibh a urna. Sodales nam mollis iaculis diam viverra. Arcu a ligula morbi tristique suscipit amet. Nibh tincidunt eget aliquet vulputate tempus quisque magna."
+};
+
+// --- Komponen Internal: Lingkaran Skor (0-4) ---
+const ScoreCircle = ({ value, selected }) => {
+    return (
+        <div className="flex flex-col items-center mx-1">
+            <span className="text-xs font-medium text-gray-600 mb-1">{value}</span>
+            <div 
+                className={`w-5 h-5 rounded-full border border-black flex items-center justify-center
+                    ${selected ? 'bg-black' : 'bg-white'}
+                `}
+            >
+                {/* Jika selected, bisa dikasih titik putih atau biarkan solid hitam */}
+                {selected && <div className="w-2 h-2 bg-white rounded-full"></div>}
+            </div>
+        </div>
+    );
+};
+
+export default function ViewNilaiDetail() {
+  // Ambil data dari props
+  const { data = mockData } = usePage().props;
+
+  return (
+    <div className="relative bg-white w-full min-h-screen flex justify-start p-os-12 font-sans overflow-hidden">
+      <Sidebar />
+
+      <main className="grid w-full p-os-8 h-fit grid-cols-1 grid-rows-[auto_1fr_auto] gap-os-14 transition-all duration-300 md:ml-20">
+        
+        {/* 1. Breadcrumb */}
+        <OsBreadCrumb 
+          className="fixed" 
+          title="OSCE / OSCE Radiologi 01-A / Detail OSCE/Detail Stase/ Penilaian Stase/Lihat Penilaian"
+          icon={<ArrowLeft className="w-5 h-5" />}
+        />
+
+        <div className="flex-1 overflow-auto">
+          
+            {/* 2. Info Mahasiswa (Card dengan border hitam) */}
+            <div className="flex items-center p-4 border border-black rounded-xl mb-6">
+                <div className="w-16 h-16 bg-[#3C2F2F] rounded-full mr-6 flex-shrink-0"></div>
+                <div className="flex flex-col space-y-1 text-sm">
+                    <div><span className="font-bold">Nama :</span> {data.mahasiswa.nama}</div>
+                    <div><span className="font-bold">NIM:</span> {data.mahasiswa.nim}</div>
+                    <div><span className="font-bold">Jurusan :</span> {data.mahasiswa.jurusan}</div>
+                </div>
+            </div>
+
+            {/* 3. Judul Penilaian */}
+            <h2 className="text-xl text-black mb-4">Penilaian Stase</h2>
+
+            {/* 4. Tabel Penilaian */}
+            <div className="border border-black rounded-xl overflow-hidden mb-6">
+                {/* Header Tabel */}
+                <div className="flex border-b border-black bg-white text-sm font-medium">
+                    <div className="w-16 p-3 text-center border-r border-black">No</div>
+                    <div className="flex-1 p-3 border-r border-black">Aspek Penilaian</div>
+                    <div className="w-64 p-3 text-center border-r border-black">Skor</div>
+                    <div className="w-32 p-3 text-center border-r border-black">Bobot</div>
+                    <div className="w-32 p-3 text-center">Nilai</div>
+                </div>
+
+                {/* Body Tabel (Looping Kategori) */}
+                {data.penilaian.map((kategori, index) => (
+                    <div key={index}>
+                        {/* Judul Kategori (A. Persiapan, B. Orientasi) */}
+                        <div className="p-3 font-bold text-sm bg-white border-b border-black">
+                            {kategori.kategori}
+                        </div>
+
+                        {/* Looping Items per Kategori */}
+                        {kategori.items.map((item) => (
+                            <div key={item.id} className={`flex border-b border-black bg-${item.id % 2 === 0 ? 'gray-200' : 'white'}`}>
+                                <div className="w-16 p-3 text-center border-r border-black flex items-center justify-center text-lg">{item.id}</div>
+                                <div className="flex-1 p-3 border-r border-black flex items-center">{item.aspek}</div>
+                                <div className="w-64 p-3 border-r border-black flex items-center justify-center">
+                                    {/* Render 5 Lingkaran Skor (0-4) */}
+                                    {[0, 1, 2, 3, 4].map((score) => (
+                                        <ScoreCircle key={score} value={score} selected={item.skor === score} />
+                                    ))}
+                                </div>
+                                <div className="w-32 p-3 border-r border-black flex items-center justify-center">{item.bobot}</div>
+                                <div className="w-32 p-3 flex items-center justify-center font-bold">{item.nilai}</div>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+
+                {/* Footer Total Nilai */}
+                <div className="flex bg-white font-bold">
+                    <div className="flex-1 p-3 border-r border-black">Total nilai aspek penilaian</div>
+                    <div className="w-32 p-3 text-center">{data.totalNilai}</div>
+                </div>
+            </div>
+
+            {/* 5. Feedback */}
+            <div className="mb-6">
+                <h3 className="text-lg text-black mb-2">Feedback</h3>
+                <div className="border border-black rounded-xl p-4 text-gray-600 text-sm leading-relaxed min-h-[150px]">
+                    {data.feedback}
+                </div>
+            </div>
+
+        </div>
+        <OsCopyright />
+      </main>
+    </div>
+  );
+}
