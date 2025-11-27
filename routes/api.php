3a45719d83dbedea7e42ff8b661c2b8d3eb0fc1b
Afkar5115 <muhammadmumtazaalafkar@gmail.com>
2025-11-27 18:29:26 +0700

    Menambahkan fungsi melihat nilai dan menginput nilai

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,31 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\ViewNilaiController;
+use App\Http\Controllers\Api\V1\InputNilaiController; // <--- Pastikan import controller baru
 
 Route::prefix('v1')->group(function () {
     Route::get('/login', function () {
         return redirect()->route('login'); 
     });
 
-    // 2. Route API Asli (POST)
     Route::post('/login', [AuthController::class, 'login']);
 
-    // Routes yang butuh Token (Protected)
     Route::middleware('auth:sanctum')->group(function () {
         
         Route::post('/logout', [AuthController::class, 'logout']);
         
-        // Cek user saat ini
         Route::get('/me', function (Request $request) {
             return $request->user();
         });
+
+        // VIEW NILAI (Sudah ada)
+        Route::get('/penilaian/{id_enrollment_osce}/view', ViewNilaiController::class);
+
+        // INPUT NILAI (Tambahkan ini)
+        // Kita gunakan POST. Sesuaikan nama controllernya nanti.
+        Route::post('/penilaian', InputNilaiController::class); 
     });
 
 });
