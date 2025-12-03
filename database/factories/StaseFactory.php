8519871bdd55ddf2b924b02ea3c82676a8b9d07e
setyanoegraha <pandu4688@gmail.com>
2025-12-03 23:07:58 +0700

    refactor(factories): update definitions to use smart parent logic and ID locale

--- database/factories/StaseFactory.php
+++ database/factories/StaseFactory.php
@@ -2,30 +2,19 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\MataKuliah;
 use App\Models\TujuanPembelajaran;
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Stase>
- */
+use Illuminate\Database\Eloquent\Factories\Factory;
+
 class StaseFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     * catatan :
-     * relasi foreignkey : mata kuliah dan tujuan pemblejaran.
-     * satu mata kuliah memiliki banyak stase.
-     * satu stase memiliki banyak tujuan pembelajaran
-     */
     public function definition(): array
     {
         return [
-            'id_mata_kuliah' => MataKuliah::factory(),
-            'id_tujuan_pembelajaran' => TujuanPembelajaran::factory(),
-            'nama_stase' => $this->faker->sentence(2),
-            'deskripsi' => $this->faker->paragraph(),
+            'id_mata_kuliah' => MataKuliah::inRandomOrder()->first()->id_mata_kuliah ?? MataKuliah::factory(),
+            'id_tujuan_pembelajaran' => TujuanPembelajaran::inRandomOrder()->first()->id_tujuan_pembelajaran ?? TujuanPembelajaran::factory(),
+            'nama_stase' => 'Stase ' . $this->faker->word(),
+            'deskripsi' => $this->faker->sentence(),
         ];
     }
 }
