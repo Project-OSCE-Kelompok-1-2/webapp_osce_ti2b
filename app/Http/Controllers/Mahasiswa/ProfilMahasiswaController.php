5c288307ec571ab0ba10e685a2e581db56b74f13
asdiffada <afada085@gmail.com>
2025-12-04 14:48:08 +0700

    nambahin pengaturan akun untuk mahasiswa

---
+++ app/Http/Controllers/Mahasiswa/ProfilMahasiswaController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use App\Services\ProfilMahasiswaService;
+use Exception;
+
+class ProfilMahasiswaController extends Controller
+{
+    protected $profilmahasiswaService;
+
+    public function __construct(ProfilMahasiswaService $profilmahasiswaService)
+    {
+        $this->profilmahasiswaService = $profilmahasiswaService;
+    }
+
+    public function show_profile()
+    {
+        $user = Auth::user();
+        
+        $user->load('mahasiswa');
+
+        $user->path_gambar = $user->path_gambar ? $user->path_gambar : null;
+
+        return Inertia::render('Mahasiswa/MahasiswaProfil', [
+            'user' => $user, 
+        ]);
+    }
+
+    public function update_account(Request $request)
+    {
+        $mahasiswa = Auth::user();
+
+        $request->validate([
+            'foto'          => ['nullable', 'image', 'mimes:jpg,jpeg,png,gif', 'max:1024'],
+            'new_password'  => ['nullable', 'string', 'min:6', 'confirmed'],
+            'old_password'  => ['nullable', 'string'], 
+            'delete_foto'   => ['nullable', 'boolean'],
+        ]);
+
+        try {
+            $this->profilmahasiswaService->updateProfile(
+                $mahasiswa, 
+                $request->all(), 
+                $request->file('foto')
+            );
+
+            return back()->with('success', 'Profil berhasil diperbarui!');
+
+        } catch (Exception $e) {
+            return back()->withErrors([
+                'old_password' => $e->getMessage()
+            ]);
+        }
+    }
+}
