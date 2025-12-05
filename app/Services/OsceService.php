ee471cfcb66ab5d724bdcda2ac372cbbf0164d39
ifad <yusuffadhlih123@gmail.com>
2025-12-05 10:36:31 +0700

    fix tanggal yang tidak tampil ketika mengedit data di frontend

--- app/Services/OsceService.php
+++ app/Services/OsceService.php
@@ -25,7 +25,7 @@ class OsceService
             });
         }
 
-        $paginator = $query->orderBy('tanggal_mulai', 'desc')->paginate(10);
+        $paginator = $query->orderBy('tanggal_mulai', 'desc')->paginate(10)->withQueryString();;
 
         // Gunakan through() untuk memetakan (map) data di dalam objek paginator
         $data = $paginator->through(function ($osce) {
@@ -33,10 +33,11 @@ class OsceService
                 "id_osce" => $osce->id_osce,
                 "nama_osce" => $osce->nama_osce,
                 "detail_stase" => $osce->detail_stase ?? "0 Stase",
+                'id_tahun_akademik' => $osce->id_tahun_akademik,
                 "detail_mahasiswa" => $osce->detail_mahasiswa ?? "0 Mahasiswa",
                 "detail_sesi" => $osce->detail_sesi ?? "0 Sesi",
-                "tanggal_mulai" => $osce->tanggal_mulai->format('d-m-Y'),
-                "tanggal_selesai" => $osce->tanggal_selesai->format('d-m-Y'),
+                "tanggal_mulai" => $osce->tanggal_mulai->format('Y-m-d'),
+                "tanggal_selesai" => $osce->tanggal_selesai->format('Y-m-d'),
                 "tahun_akademik_string" => $osce->tahunAkademik->tahun ?? "N/A",
             ];
         });
