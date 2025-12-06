64235b5b2158e283ab724d7c7ca1327c373d0fe7
Radityaz <rikoadityazaki@gmail.com>
2025-12-06 13:40:14 +0700

    membersihkan beberapa layout

--- resources/js/pages/Penguji/PengujiDashboard.jsx
+++ resources/js/pages/Penguji/PengujiDashboard.jsx
@@ -179,7 +179,7 @@ export default function PengujiDashboard() {
                             Statistika
                         </h2>
                     </div>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                         {/* [UBAH] Tambahkan prop 'href' di sini */}
                         <StatCard
                             title="OSCE Mendatang"
