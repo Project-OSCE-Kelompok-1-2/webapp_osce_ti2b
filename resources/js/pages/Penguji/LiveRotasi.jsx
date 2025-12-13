32c62ed8e8d6097103b358d8350837231c56f145
tommystar <muhammadbintangsat@gmail.com>
2025-12-13 17:53:40 +0700

    perbaikan pada alur penilaian mahasiswa menjadi ke submitrubrik

--- resources/js/pages/Penguji/LiveRotasi.jsx
+++ resources/js/pages/Penguji/LiveRotasi.jsx
@@ -24,7 +24,7 @@ export default function LiveRotasi() {
         if (isFinished) {
             // Jika habis, tutup sesi
             router.get(
-                `/penguji/osce/${safeOsce.id_osce}/stase/${safeOsce.id_osce_stase}/rekap`
+                `/penguji/osce/${safeOsce.id_osce}/stase/${safeOsce.id_osce_stase}/submitrubrik`
             );
         } else {
             // Jika ada, lanjut nilai
