656d74e45566efb98ae80ad2c94dc579da3684be
tommystar <muhammadbintangsat@gmail.com>
2025-12-13 18:17:39 +0700

    resolve routes submitrubrik

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,7 @@ Route::prefix('penguji')->middleware(['auth', 'role:penguji'])->name('penguji.')
     Route::get('/penilaian/{id_enrollment_osce}', [HalamanPenilaianController::class, 'showPenilaian'])->name('penilaian.show');
     Route::post('/penilaian/{id_enrollment_osce}', [AksiPenilaianController::class, 'store'])->name('penilaian.store');
     Route::get('/penilaian/{id_enrollment_osce}/nilai', [AksiPenilaianController::class, 'getNilai'])->name('penilaian.getNilai');
-    Route::get('/osce/{id_osce}/stase/{id_osce_stase}/submitrubrik', [AksiPenilaianController::class, 'submitRubrik'])->name('penilaian.submitrubrik');
+    Route::get('/osce/{id_osce}/stase/{id_osce_stase}/submitrubrik',[AksiPenilaianController::class, 'submitRubrik'])->name('Penilaian.submitrubrik');
 
 
     // --- ALUR PASCA UJIAN / REKAP (PENGUJI) ---
