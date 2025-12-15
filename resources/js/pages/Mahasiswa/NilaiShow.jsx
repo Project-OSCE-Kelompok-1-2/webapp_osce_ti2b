c87bb85f521ed8e5caf33122300dcefff507f9be
Ray Egans <egansray@gmail.com>
2025-12-15 11:38:06 +0700

    Fix: Memperbaiki grading nilai dan header variant goback pada mahasiswa

--- resources/js/pages/Mahasiswa/NilaiShow.jsx
+++ resources/js/pages/Mahasiswa/NilaiShow.jsx
@@ -115,6 +115,7 @@ export default function NilaiShow({ header_detail, daftar_nilai, footer }) {
                     <OsHeader
                         onMenuClick={() => setIsSidebarOpen(!isSidebarOpen)}
                         variant="goback"
+                        role="mahasiswa"
                     />
 
                     {/* KONTEN UTAMA */}
